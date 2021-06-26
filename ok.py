97a1dffef0bbf3ec1f5a46dd01ed0c99ff32c4ea
waqas7070 <85313636+waqas7070@users.noreply.github.com>
2021-06-26 18:53:03 +0500

    Create ok.py

---
+++ ok.py
@@ -0,0 +1,9 @@
+# Decompiled By RandiSr
+# Github : https://github.com/RANDIOLOY
+# uncompyle6 version 3.7.4
+# Python bytecode 2.7
+# Decompiled from: Python 2.7.18 (default, Mar 20 2021, 14:59:33) 
+# [GCC 4.2.1 Compatible Android (6454773 based on r365631c2) Clang 9.0.8 (https:/
+# Embedded file name: Sumarr ID
+import bhottool
+bhottool.menu()
